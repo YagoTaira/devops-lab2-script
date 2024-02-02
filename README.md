ab32932b4e7aed6bf3d567838235d6ac75be4d3d
Yago Taira <yagotaira@gmail.com>
2024-02-02 09:57:39 +0000

    Script test commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Week 2 Lab - Bash & Git
+Yago Taira x19238568
+[devops-lab2-test-script](https://github.com/YagoTaira/devops-lab2-test-script.git)
+---
+To run the script.sh run: `bash script.sh`
--- script.sh
+++ script.sh
@@ -17,9 +17,26 @@ touch .gitignore
 echo ".gitignore" >> .gitignore
 echo fol_1 >> .gitignore
 echo fol_2 >> .gitignore
+
+chmod 600 fol_1/1_1.txt
+chmod 600 fol_1/1_3.txt
+chmod 600 fol_2/2_1.txt
+chmod 600 fol_2/2_3.txt
+chmod 000 fol_1/1_2.txt
+chmod 000 fol_2/2_2.txt
+
+touch README.md
+echo "# Week 2 Lab - Bash & Git" > README.md
+echo "Yago Taira x19238568" >> README.md
+echo "[devops-lab2-test-script](https://github.com/YagoTaira/devops-lab2-test-script.git)" >> README.md
+echo "---" >> README.md
+echo "To run the script.sh run: \`bash script.sh\`" >> README.md
+
 git init
 git add .
-git commit -m "First commit"
-git remote add origin https://github.com/YagoTaira/devops-lab2-test-script.git
+git commit -m "Script test commit"
+git remote add origin https://github.com/YagoTaira/devops-lab2-script.git
 git branch -M main
 git push -u origin main
+
+echo Job Completed
